bf7342d6921d8aca7cf89a48fa1e8beb95274a22
Dhruv Boghani <dhruvboghani624@gmail.com>
2026-02-20 09:52:05 +0530

    RUNTIME error in index.js line 0 -> Fix: Added .js extension to Router import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Router = require('./router'); // Removed .js extension
+const Router = require('./router.js'); // Added .js extension
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const path = require("path");
