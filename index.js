04bcf7d4c292a3f8f6da82a20f41143530d37342
Dhruv Boghani <dhruvboghani624@gmail.com>
2026-02-20 09:51:22 +0530

    RUNTIME error in index.js line 0 -> Fix: Corrected CORS configuration option name from 'Credential' to 'credentials'.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const path = require("path");
 const cors = require('cors');
 const corsConfig = {
     origin: "*",
-    Credential: true,
-    method: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }
 
